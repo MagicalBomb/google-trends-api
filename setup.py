593af6517516deb951143e85db1db05a8fc0ffa0
1.5W PC <17826800084g@gmail.com>
2022-02-27 10:15:38 +0800

    modify version -> 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='google-trends-api',
-    version='1.0.0',
+    version='1.0.1',
     packages=['google_trends_api'],
     # package_dir={'': ''},
     url='',
