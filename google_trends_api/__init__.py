1868efeea021ae28ce358075c40296ceba5927c0
下雨天玩游戏 <17826800084g@gmail.com>
2022-02-28 17:04:40 +0800

    rename old hourly_data -> _hourly_data and fix a bug of it

--- google_trends_api/__init__.py
+++ google_trends_api/__init__.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timezone, timedelta
 
 from google_trends_api import constants, utils, _api
+from google_trends_api.utils import datetime_range, alist
 
 
-async def hourly_data(
+async def _hourly_data(
         keyword: str,
         start_datetime: datetime,
         end_datetime: datetime,
@@ -13,7 +14,8 @@ async def hourly_data(
         host_language: str = "en-US",
 ):
     """
-    Get hourly google trends data for a keyword between start_datetime and end_datetime.
+    Get hourly google trends data for a keyword in every 7 day period.
+    Note hourly trends value is relative to their period, not a absolute value.
 
     :param keyword: The keyword to get data for.
     :param start_datetime: The start datetime to get data for.
@@ -60,6 +62,47 @@ async def hourly_data(
 
         for item in hourly_items:
             timestamp = int(item['time'])
-            if item['hasData'][0] and datetime.fromtimestamp(timestamp, tz=tzinfo) <= (end_datetime - timedelta(minutes=timezone_offset)):
+            if item['hasData'][0] and datetime.fromtimestamp(timestamp, tz=tzinfo) < (end_datetime - timedelta(minutes=timezone_offset)):
                 yield timestamp, item['value'][0]
 
+
+async def seven_days_data(
+    keyword: str,
+    start_datetime: datetime,
+    tzinfo: timezone = None,
+    *,
+    geo: str = "",
+    host_language: str = "en-US",
+):
+    """
+    Get 7 day google trends data for a keyword
+
+    :param keyword: The keyword to get data for.
+    :param start_datetime: The start datetime to get data for.
+    :param tzinfo: The timezone to get data for. Defaults to local timezone.
+
+    @return: yield (timestamp, value)
+    """
+
+    async for time, value in _hourly_data(keyword, start_datetime, start_datetime + timedelta(days=7), tzinfo=tzinfo, geo=geo, host_language=host_language):
+        yield time, value
+
+
+async def hourly_data(
+        keyword: str,
+        start_datetime: datetime,
+        end_datetime: datetime,
+        tzinfo: timezone = None,
+        *,
+        geo: str = "",
+        host_language: str = "en-US",
+):
+    """
+    Get hourly google trends data for a keyword.
+    Note hourly trends value is relative to the first 7 days of the start_datetime
+    """
+    timedelta_7days = timedelta(days=7)
+    timedelta_1hour = timedelta(hours=1)
+
+    first_7days = await alist(seven_days_data(keyword, start_datetime, tzinfo, geo=geo, host_language=host_language))
+    second_7days = await alist(seven_days_data(keyword, start_datetime + timedelta_7days, tzinfo, geo=geo, host_language=host_language))
--- tests/test_google_trends_api.py
+++ tests/test_google_trends_api.py
@@ -2,16 +2,17 @@ from datetime import datetime, timezone, timedelta
 
 import pytest
 
-from google_trends_api import hourly_data
+from google_trends_api import _hourly_data, seven_days_data
+from google_trends_api.utils import alist
 
 
 @pytest.mark.asyncio
-async def test_hourly_data():
+async def test__hourly_data():
     start_datetime = datetime(2022, 1, 10, 10)
     end_datetime = datetime(2022, 1, 13, 14)
     tzinfo = timezone(timedelta(hours=6))
     item_list = []
-    async for timestamp, value in hourly_data(
+    async for timestamp, value in _hourly_data(
         keyword='nft',
         start_datetime=start_datetime,
         end_datetime=end_datetime,
@@ -24,3 +25,15 @@ async def test_hourly_data():
 
     last_item = item_list[-1]
     assert datetime.fromtimestamp(last_item[0], tz=tzinfo) == datetime(2022, 1, 13, 14, tzinfo=tzinfo)
+
+
+@pytest.mark.asyncio
+async def test_seven_days_data():
+    lst = await alist(seven_days_data(
+        keyword='nft',
+        start_datetime=datetime(2020, 1, 1, 1),
+        tzinfo=timezone(timedelta(hours=8)),
+    ))
+
+    assert lst[0][0] == datetime(2020, 1, 1, 1).timestamp()
+    assert lst[-1][0] == datetime(2020, 1, 8, 0).timestamp()
